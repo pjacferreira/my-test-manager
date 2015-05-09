4ac971949285405b67d7dd0de6e60f4187e68bc7
Paulo Ferreira <pf@sourcenotes.org>
2015-05-09 15:56:18 +0100

    Added new Landing Pages (Introduction and Help) and rewrote the menus.

---
+++ client/ui/private/pages/landing/introduction.php
@@ -0,0 +1,53 @@
+<!-- 
+ --- copyright 2015 Paulo Ferreira <pf at sourcenotes.org>
+ --- license http://opensource.org/licenses/AGPL-3.0 Affero GNU Public License v3.0
+-->
+<h1>Welcome to TestCenter</h1>
+<h2>Introduction</h2>
+<p>TestCenter aims to be the way you perform <a href="https://en.wikipedia.org/wiki/Acceptance_testing">UAT</a> or <a href="http://en.wikipedia.org/wiki/Functional_testing">Functional</a> for all types of projects (whether it be for a software project or the test phase of some system's/project's implementation).</p>
+<p>The idea is simple. You create you:</p>
+<ol>
+  <li>Create Tests.</li>
+  <li>Each Test, has as a series of Test Steps, that have to be executed in series.</li>
+  <li>You sequence tests, as Test Sets, in order to test a particular feature of the implementation.</li>
+  <li>You, or some other user, Runs a Test Set, and registers the result of the execution (of the tests and test steps).</li>
+  <li>The system maintains a list of the runs and provides statistics on pass/failure of Tests and Test Sets.</li>
+</ol>
+<p>But first, we have to complete the implementation of TestCenter.</p>
+<h2>Current State</h2>
+<p>TestCenter is still under development....</p>
+<p>The site you are seeing is an early alpha version (not all features implemented) site that I have made available for 3 reasons:</p>
+<ol>
+  <li>Allow other people to see, and try out, an early version of the application.</li>
+  <li>Allow people to start participating, early on, in the project development. Be it through testing the system, ideas for the design or features they would like to see (i.e. get in on the ground floor of the development, and allow you to orient, within reasonable limits, the projects direction).</li>
+</ol>
+<p><strong>Note:</strong> </p>
+<ul>
+  <li>This test site is being hosted on a very limited piece of hardware I had lying around, so don't expect too much in the way of performance.</li>
+  <li>Please don't deliberately try to destroy the system. Even though I can reset it's state with relative ease, I would still prefer to take away as little time from development as possible (But if you find a serious flaw in the security configuration of the system please tell me).</li>
+</ul>
+<h2>How can you use the site?</h2>
+<p>So can you use the site?</p>
+<ol>
+  <li>Go back to the home page (i.e. Click the Home in the Main Menu).</li>
+  <li>Login in to the System (i.e. Click the Login Action), and use 'admin'/'admin' as user and password in the login form and click Login.</li>
+  <li>Navigate the system, creating tests / steps / sets or whatever is available for you to play with.</li>
+</ol>
+<h2>How can you participate?</h2>
+<p>So how can you participate?</p>
+<ol>
+  <li>Well the source code is hosted at <a href="https://github.com/pjacferreira/testcenter">GitHUB</a> so please feel free to browse it (Please note that, I use my own internal development system, so I only push my code to GitHUB when I have made significant changes).</li>
+  <li>I will always try to base the site on a code released to GitHUB.</li>
+  <li>PLEASE Register any <a href="https://github.com/pjacferreira/testcenter/issues">issues</a> on GitHUB.</li>
+</ol>
+<h2>What's currently missing?</h2>
+<ol>
+  <li>A page on this site with a Change Log, so that you can easily see what new.</li>
+  <li>Ability to Run a Test Set (Next feature to be added).</li>
+  <li>Administration Interface (i.e. the Ability to Manage Users, Organizations, Projects).</li>
+  <li>Too many other things....</li>
+</ol>
+<h2>For your information</h2>
+<ol>
+  <li>The system is rebooted every hour, on the hour, to preserve resources. SO PLEASE EXPECT sessions cut offs.</li>
+</ol>
---
+++ client/ui/private/pages/landing/menus/menu.php
@@ -0,0 +1,51 @@
+<!-- 
+ --- copyright 2015 Paulo Ferreira <pf at sourcenotes.org>
+ --- license http://opensource.org/licenses/AGPL-3.0 Affero GNU Public License v3.0
+-->
+<div id="sb_menu" class="ui left sidebar vertical pointing menu" style="overflow: hidden; background-color: green">
+  <div class="item">
+    <h3><?php echo _("MENU:GROUP:ACTIONS") ?></h3>
+    <div class="pointing menu">
+      <a name="home" class="item" href=<?php echo "'" . $this->url->get('page/landing:home') . "'" ?> >
+        <i class="home icon"></i><?php echo _("MENU:HOME") ?>
+      </a>
+    </div>
+  </div>
+  <!--
+    <div class="item">
+      <h3><?php echo _("MENU:GROUP:CONDITIONS") ?></h3>
+      <div class="pointing menu">
+        <a name="use" class="item" href=<?php echo "'" . $this->url->get('page/landing:use') . "'" ?> >
+          <i class="content icon"></i><?php echo _("MENU:CONDITIONS:USE") ?>
+        </a>
+        <a name="privacy" class="item" href=<?php echo "'" . $this->url->get('page/landing:privacy') . "'" ?> >
+          <i class="privacy icon"></i><?php echo _("MENU:CONDITIONS:PRIVACY") ?>
+        </a>
+      </div>
+    </div>
+    <div class="item">
+      <h3><?php echo _("MENU:GROUP:US") ?></h3>
+      <div class="pointing menu">
+        <a name="contact" class="item" href=<?php echo "'" . $this->url->get('page/landing:contact') . "'" ?> >
+          <i class="mail icon"></i><?php echo _("MENU:US:CONTACT") ?>
+        </a>
+        <a name="about" class="item" href=<?php echo "'" . $this->url->get('page/landing:about') . "'" ?> >
+          <i class="info circle icon"></i><?php echo _("MENU:US:ABOUT") ?>
+        </a>
+      </div>
+    </div>
+  -->
+  <div class="item">
+    <h3><?php echo _("MENU:GROUP:HELP") ?></h3>
+    <div class="pointing menu">
+      <a name="help" class="item" href=<?php echo "'" . $this->url->get('page/landing:introduction') . "'" ?> >
+        <i class="help icon"></i><?php echo _("MENU:HELP:INTRODUCTION") ?>
+      </a>
+    </div>
+    <div class="pointing menu">
+      <a name="help" class="item" href=<?php echo "'" . $this->url->get('page/landing:help') . "'" ?> >
+        <i class="help icon"></i><?php echo _("MENU:HELP:HELP") ?>
+      </a>
+    </div>
+  </div>
+</div>
