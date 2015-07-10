76d1b398424b15ad24269b35f25872b32d3c1ccd
Paulo Ferreira <pf@sourcenotes.org>
2015-07-10 10:41:27 +0100

    Forget to do I18N (en) for Player Page.

--- client/ui/private/pages/user/player.php
+++ client/ui/private/pages/user/player.php
@@ -7,18 +7,18 @@
 -->
 <!-- CREATE FOLDER FORM -->
 <div id="form_step_complete" class="tc ui form hidden">
-  <h4 class="ui dividing header"><?php echo _("FORM:TITLE:STEP:COMPLETE") ?></h4>
+  <h3 class="ui dividing inverted header"><?php echo _("FORM:TITLE:PASS:STEP") ?></h3>
   <div class="field">
     <select name="pass_code" class="ui search dropdown">
-      <option value=""><?php echo _("PLACEHOLDER:PASS:CODE") ?></option>
+      <option value=""><?php echo _("FIELD:PLACEHOLDER:PASS:CODE") ?></option>
     </select>
   </div>
   <div class="field">
-    <textarea name="comment" placeholder="<?php echo _("FIELD:COMMENT") ?>" type="text"></textarea>
+    <textarea name="comment" placeholder="<?php echo _("FIELD:PLACEHOLDER:PASS:COMMENT") ?>" type="text"></textarea>
   </div>
   <div class="ui buttons">
     <div id="button_step_complete" class="ui positive button"><?php echo _("BUTTON:STEP:PASS") ?></div>
-    <div class="or"></div>
+    <div class="<?php echo _("HELPER:CLASS:OR") ?>"></div>
     <div id="button_cancel" class="ui negative button"><?php echo _("BUTTON:CANCEL") ?></div>
   </div>
   <div class="ui error message"></div>
@@ -26,36 +26,36 @@
 <!-- MAIN PAGE -->
 <div id="navigator">
   <h3 class="ui top attached centered inverted header">
-    <?php echo _("PAGE:USER:RUNS:NAVIGATOR") ?>
+    <?php echo _("PAGE:SECTION:RUNS:NAVIGATOR") ?>
   </h3>
   <div class="ui attached segment">
     <div class="ui divided grid">
       <div id="folders" class="four wide column">
         <h3 class="ui header centered inverted" style="background-color: black">
-          <?php echo _("PAGE:USER:RUNS:FOLDERS") ?>
+          <?php echo _("NAVIGATOR:PANE:FOLDERS") ?>
         </h3>
       </div>
       <div id="list_runs" class="twelve wide column">
         <h3 class="ui header centered inverted" style="background-color: blue">
-          <?php echo _("PAGE:USER:RUNS:RUNS") ?>
+          <?php echo _("NAVIGATOR:PANE:RUNS") ?>
         </h3>
       </div>
     </div>
   </div>
   <h3 class="ui centered attached inverted header">
-    <?php echo _("PAGE:USER:PLAYER") ?>
+    <?php echo _("PAGE:SECTION:PLAYER:DETAILS") ?>
   </h3>
   <div id='player' class="ui stackable column grid player segment">
     <div class="six wide column">
       <h3 class="ui centered header">
-        <?php echo _("PAGE:USER:PLAYER:TEST") ?>
+        <?php echo _("PAGE:SECTION:PLAYER:TESTS") ?>
       </h3>
       <div id="test_cards" class="ui">
       </div>
     </div>
     <div class="ten wide column">
       <h3 class="ui centered header">
-        <?php echo _("PAGE:USER:PLAYER:STEPS") ?>
+        <?php echo _("PAGE:SECTION:PLAYER:STEPS") ?>
       </h3>
       <div id="step_cards" class="ui">
       </div>
